b512002563225891ed2f2ec15a901773fce6ef2d
Vaidehi Desai <vaidesai171202@gmail.com>
2022-05-21 16:48:47 +0530

    Create 75-sort-colors.cpp

---
+++ Day1/75-sort-colors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int z = 0;
+        int o = 0;
+        int t = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] == 0) z++;
+            if(nums[i] == 1) o++;
+            if(nums[i] == 2) t++;
+        }
+        for(int i = 0; i < z; i++)
+            nums[i] = 0;
+        for(int i = z; i < z + o; i++)
+            nums[i] = 1;
+        for(int i = z+o; i < z + o + t; i++)
+            nums[i] = 2;
+    }
+};
