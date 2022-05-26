13c986f5624ab743a2e98d445f36efa44e4f5a13
Vaidehi Desai <vaidesai171202@gmail.com>
2022-05-26 12:23:42 +0530

    Create 20-valid-parentheses.cpp

---
+++ Day6/20-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+                st.push(s[i]);
+            else if(s[i] == ')' && st.size() && st.top() == '(')
+                st.pop();
+            else if(s[i] == ']' && st.size() && st.top() == '[')
+                st.pop();
+            else if(s[i] == '}' && st.size() && st.top() == '{')
+                st.pop();
+            else
+                return false;
+        }
+        if(st.size() == 0)
+            return true;
+        return false;
+    }
+};
