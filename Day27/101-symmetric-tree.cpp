bcce5cff6d163b87edfd327ac6b07a8160a51095
Vaidehi Desai <vaidesai171202@gmail.com>
2022-06-16 18:44:14 +0530

    Create 101-symmetric-tree.cpp

---
+++ Day27/101-symmetric-tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSym(TreeNode* root1, TreeNode* root2)
+    {
+        if(!root1 && !root2)
+            return true;
+        else if(root1 && root2 && root1->val == root2->val)
+            return isSym(root1->left, root2->right) && isSym(root1->right, root2->left);
+        return false;
+    }
+    bool isSymmetric(TreeNode* root) 
+    {
+        return isSym(root, root);
+    }
+};
