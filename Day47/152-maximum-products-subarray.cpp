d2cd38bd50fa389c35de774a1ea9f958535260ba
Vaidehi Desai <vaidesai171202@gmail.com>
2022-07-07 15:11:27 +0530

    Create 152-maximum-products-subarray.cpp

---
+++ Day47/152-maximum-products-subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) 
+    {
+        int prod = 1, ans = INT_MIN;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            prod *= nums[i];
+            ans = max(prod, ans);
+            if(prod == 0)
+                prod = 1;
+        }
+		prod = 1;
+		for(int i = nums.size()-1; i >= 0; i--)
+        {
+			prod *= nums[i];
+			ans = max(prod, ans);
+			if(prod == 0)
+				prod = 1;
+		}
+        
+		return ans;
+    }
+};
